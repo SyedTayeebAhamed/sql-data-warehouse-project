b1430b5369ed13edd34d8c113e999f5a7f526269
SyedTayeebAhamed <152951053+SyedTayeebAhamed@users.noreply.github.com>
2025-11-18 16:55:19 +0530

    Add foreign key integrity check SQL query
    Add SQL query to check foreign key integrity for gold sales.

---
+++ tests/quality_check_gold.sql
@@ -0,0 +1,8 @@
+    ---Foreign key Integrity
+    select 
+    * from gold.fact_sales f
+    left join gold.dim_customers c
+    on c.customer_key =f.customer_key
+    left join gold.dim_product p
+    on p.product_key=f.product_key
+    where p.product_key is null
